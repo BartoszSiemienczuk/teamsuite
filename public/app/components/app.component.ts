824d2a91faa335f6b178014cb13402cf2ad13fea
siemienczuk.bartosz <siemienczuk.bartosz@gmail.com>
2016-09-19 04:27:16 -0400

    Loading indicator

--- public/app/components/app.component.ts
+++ public/app/components/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'teamsuite-app',
   templateUrl: '/app/views/master.html'
 })
 
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(){ };
+  
+  ngOnInit(){ };
+  ngOnDestroy(){ };
+}
