59e2385db8ee274d2d7d2f835fe20f89b9d7c5f1
siemienczuk.bartosz <siemienczuk.bartosz@gmail.com>
2016-09-23 04:57:56 -0400

    [FEATURE] Basic JWT usage for auth

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as User from '../model/user';
+var expressJwt = require ('express-jwt');
+var jwt = require ('jsonwebtoken');
 
 let secret = "aJD39xmZA8b24uD9qfkddmH3yk5dcZjw";
 let router = express.Router();
@@ -9,20 +11,33 @@ router = express.Router();
     
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}));
-
+router.use(expressJwt({secret : secret}).unless({path : ['/auth/login']}));
 
 router.post('/login', (req : express.Request, res : express.Response) => {
   console.log("Login attempt : "+req.body.login +"\n posted.");
   User.findOne({login: req.body.login}, (err, user) => {
-    user.comparePassword(req.body.password, (err, matches) => {
-      if(err) throw err;
-      if(!matches){
-        res.status(401).send("Not authenticated");
-      } else {
-        res.status(200).json("OK");
-      }
-    });
+    if(!user){
+      res.status(401).json({error:"bad_login"});
+    } else {
+      user.save(); //rehash password
+      user.comparePassword(req.body.password, (err, matches) => {
+        if(err){
+          console.log(err);
+        }
+        if(!matches){ //bad credentials
+          res.status(401).json({error:"bad_password"});
+        } else { //credentials match, lets login
+          let token_ = jwt.sign({user:user}, secret);
+          res.status(200).json({error:null,token:token_});
+        }
+      });
+    }
   });
+});
+
+router.get('/user', (req: express.Request, res : express.Response) => {
+  console.log("User data expected.");
+  res.status(200).send("OK");
 });
 
 export { router as authRoutes};
