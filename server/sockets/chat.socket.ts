eac88647f21b1e73ecb578eeaef90a974cd8ea4c
Bartosz Siemieńczuk <siemienczuk.bartosz@gmail.com>
2017-01-29 12:56:22 +0000

    [FIX] Fixed multiple users socket bug.

--- server/sockets/chat.socket.ts
+++ server/sockets/chat.socket.ts
@@ -2,78 +2,62 @@ import {User, ChatMessage} from '../model';
 export class ChatSocket {
     connectedUsers: any[][] = [];
     allConnectedUsers: any[] = [];
-    chatConnection: any;
-    uploader: any;
 
     constructor(private socketIO: any) {
         this.socketIO.of('/sockets/chat').on('connection', (con: any) => {
-            this.chatConnection = con;
 
-            this.listen();
+            this.listen(con);
         });
         console.log("Chat socket listening at /sockets/chat/");
     }
 
-    private listen(): void {
-        this.chatConnection.on("message", (data: any) => {
+    private listen(socket): void {
+        socket.on("message", (data: any) => {
             let msg = new ChatMessage({text: data.text, user: data.user, room: data.room});
             msg.save();
             msg.populate({path: 'user', select: 'login name'}, (err, data: any) => {
                 //broadcast message with populated user data
-                this.chatConnection.broadcast.to(data.room).emit("message", data);
-                this.chatConnection.emit("message", data);
+                socket.broadcast.to(data.room).emit("message", data);
+                socket.emit("message", data);
             });
         });
 
-        this.chatConnection.on("login", (user: any) => {
-            this.chatConnection.join(user.room);
+        socket.on("login", (user: any) => {
+            socket.join(user.room);
             this.allConnectedUsers[user._id] = user;
             if(this.connectedUsers[user.room]==undefined){
                 this.connectedUsers[user.room] = new Array();
             }
             this.connectedUsers[user.room][user._id] = user;
-            this.chatConnection.in(user.room).emit("userlist", this.getSimpleUserList(user.room));
-            this.chatConnection.emit("userlist", this.getSimpleUserList(user.room));
-            this.sendServerMessage("User " + user.name + " joined this room.", user.room);
+            socket.in(user.room).emit("userlist", this.getSimpleUserList(user.room));
+            socket.emit("userlist", this.getSimpleUserList(user.room));
+            this.sendServerMessage("User " + user.name + " joined this room.", user.room, socket);
 
         });
 
-        this.chatConnection.on("logout", (user: any) => {
+        socket.on("logout", (user: any) => {
             var room = this.allConnectedUsers[user._id].room;
             delete this.allConnectedUsers[user._id];
             delete this.connectedUsers[room][user._id];
-            this.sendServerMessage("User " + user.name + " has disconnected.", room);
-            this.chatConnection.broadcast.to(room).emit("userlist", this.getSimpleUserList(room));
-            this.chatConnection.leave(room);
+            this.sendServerMessage("User " + user.name + " has disconnected.", room, socket);
+            socket.broadcast.to(room).emit("userlist", this.getSimpleUserList(room));
+            socket.leave(room);
         });
 
-        this.chatConnection.on("disconnect", () => { });
+        socket.on("disconnect", () => { });
     }
 
-    private sendServerMessage(text: string, room: string): void {
-        if (this.chatConnection) {
+    private sendServerMessage(text: string, room: string, socket:any): void {
+        if (socket) {
             let message = {
                 text: text,
                 room: room,
                 user: {_id: 0, login: 'server', name: 'Server'},
                 created: new Date()
             };
-            this.chatConnection.broadcast.to(room).emit("message", message);
-            this.chatConnection.emit("message", message);
+            socket.broadcast.to(room).emit("message", message);
+            socket.emit("message", message);
         }
-    }
-
-    private getRoomCount(room: string) {
-        if (this.socketIO.nsps['/sockets/chat'].adapter.rooms[room])
-            return this.socketIO.nsps['/sockets/chat'].adapter.rooms[room].length;
-        else return 0;
-    }
-
-    private sendData() {
-        var data = this.socketIO.nsps['/sockets/chat'].adapter.sids[this.chatConnection.id];
-
-        this.chatConnection.emit("debug", data);
-
     }
 
     private getSimpleUserList(room: string) {
--- server/sockets/drawing.socket.ts
+++ server/sockets/drawing.socket.ts
@@ -6,43 +6,42 @@ export class DrawingSocket {
 
     constructor(private socketIO: any) {
         this.socketIO.of('/sockets/drawing').on('connection', (con: any) => {
-            this.drawingConnection = con;
-            this.listen();
+            this.listen(con);
         });
         console.log("Drawing socket listening at /sockets/drawing/");
     }
 
-    private listen(): void {
-        this.drawingConnection.on("drawing", (data: any) => {
-            this.drawingConnection.to(data.room).emit("draw", data);
+    private listen(socket): void {
+        socket.on("drawing", (data: any) => {
+            socket.to(data.room).emit("draw", data);
         });
 
 
-        this.drawingConnection.on("clear", (data: any) => {
-            this.drawingConnection.to(data.room).emit("clear", data);
+        socket.on("clear", (data: any) => {
+            socket.to(data.room).emit("clear", data);
         });
 
-        this.drawingConnection.on("login", (user: any) => {
-            this.drawingConnection.join(user.room);
+        socket.on("login", (user: any) => {
+            socket.join(user.room);
             this.allConnectedUsers[user._id] = user;
             if(this.connectedUsers[user.room]==undefined){
                 this.connectedUsers[user.room] = new Array();
             }
             this.connectedUsers[user.room][user._id] = user;
-            this.drawingConnection.to(user.room).emit("userlist", this.getSimpleUserList(user.room));
-            this.drawingConnection.emit("userlist", this.getSimpleUserList(user.room));
+            socket.to(user.room).emit("userlist", this.getSimpleUserList(user.room));
+            socket.emit("userlist", this.getSimpleUserList(user.room));
 
         });
 
-        this.drawingConnection.on("logout", (user: any) => {
+        socket.on("logout", (user: any) => {
             var room = this.allConnectedUsers[user._id].room;
             delete this.allConnectedUsers[user._id];
             delete this.connectedUsers[room][user._id];
-            this.drawingConnection.broadcast.to(room).emit("userlist", this.getSimpleUserList(room));
-            this.drawingConnection.leave(room);
+            socket.broadcast.to(room).emit("userlist", this.getSimpleUserList(room));
+            socket.leave(room);
         });
 
-        this.drawingConnection.on("disconnect", () => {
+        socket.on("disconnect", () => {
             //leave room and such
         });
     }
